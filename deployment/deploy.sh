a1422946cf5ee242ffb30fbdedc60f4b0a57b5a3
Thilina Manamgoda <maanafunedu@gmail.com>
2019-01-06 02:23:05 +0530

    Monitoring

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,6 +6,7 @@ ${KUBECTL} create namespace wso2
 ${KUBECTL} create configmap logstash-conf --from-file=../configs/logstash/logstash.conf -n wso2
 ${KUBECTL} create configmap logstash-yml --from-file=../configs/logstash/logstash.yml -n wso2
 ${KUBECTL} create configmap kibana-yml --from-file=../configs/kibana/kibana.yml -n wso2
+${KUBECTL} create configmap metricbeat-yml --from-file=../configs/metricbeat/metricbeat.yml -n wso2
 ${KUBECTL} create -f centralized-logging-deployment.yaml -n wso2
 ${KUBECTL} create -f wso2apim-deployment.yaml -n wso2
 
